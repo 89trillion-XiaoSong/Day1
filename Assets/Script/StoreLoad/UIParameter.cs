0fa9c74dc8bd30858ddb2bf61298451d921074e8
89trillion-XiaoSong <xiaosong@89trillion.com>
2021-05-07 15:31:03 +0800

    some changes

---
+++ Assets/Script/StoreLoad/UIParameter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIParameter : MonoBehaviour
+{
+    public Text costGold;
+    public Image cardImage;
+    public Text cardNumber;
+    public Text titleText;
+}
---
+++ Assets/Script/数据读取/JsonModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonModel<T>       //用于加载Json文件数据
+{
+    public List<T> dailyProduct;
+}
